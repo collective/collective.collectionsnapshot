c25e57a84d7779c7acfd193ef440cc4934595443
Maurits van Rees <maurits@vanrees.org>
2012-09-12 23:31:34 +0200

    docs/HISTORY.txt -> CHANGES.rst

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, find_packages
-import os
 
 version = '1.0'
 
 setup(name='collective.collectionsnapshot',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=(open("README.txt").read() + "\n" +
+                        open("CHANGES.rst").read()),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          ],
       keywords='',
       author='',
       author_email='',
-      url='http://svn.plone.org/svn/collective/collective.collectionsnapshot',
+      url='https://github.com/collective/collective.collectionsnapshot',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
